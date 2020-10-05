95321544052188a17cb769568d8e3547d47bd35c
Sooraj Sahoo <ssahoo@overstock.com>
2020-10-05 13:00:56 -0600

    Created the spring boot project

---
+++ src/main/java/com/wcf/insurance/featurerequestapp/FeatureRequestAppApplication.java
@@ -0,0 +1,12 @@
+package com.wcf.insurance.featurerequestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureRequestAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FeatureRequestAppApplication.class, args);
+  }
+}
